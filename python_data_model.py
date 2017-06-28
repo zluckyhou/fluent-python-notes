d5ad3952ba2d3b8ef09e5c461ac9bcc00e4e666f
zlucky <zluckyhou@163.com>
2017-06-28 20:23:26 +0800

    add python_data_model.py

---
+++ python_data_model.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+import collections
+
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
+
+class FrenchDeck:
+    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
