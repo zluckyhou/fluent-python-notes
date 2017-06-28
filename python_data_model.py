ef901625b7a3dc1c5eb1ea27f5250aa850915ac2
zlucky <zluckyhou@163.com>
2017-06-28 23:08:46 +0800

    test git in pycharm

--- python_data_model.py
+++ python_data_model.py
@@ -5,5 +5,23 @@ import collections
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
 
-class FrenchDeck:
+class FrenchDeck(object):
     ranks = [str(n) for n in range(2, 11)] + list('JQKA')
+    suits = 'spades diamonds clubs hearts'.split()
+
+    def __init__(self):
+        self._cards = [Card(rank, suit) for suit in self.suits
+                       for rank in self.ranks]
+
+    def __len__(self):
+        return len(self._cards)
+
+    def __getitem__(self, position):
+        return self._cards[position]
+
+
+deck = FrenchDeck()
+
+suits_values = dict(spades=3, diamonds=1, hearts=2, clubs=0)
+for i in suits_values.items():
+    print(i, i[0], i[1])
