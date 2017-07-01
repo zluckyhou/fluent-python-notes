59276e8404459c2ac8c60a86d0b95a23d5fc648b
zlucky <zluckyhou@163.com>
2017-07-01 23:42:00 +0800

    add some examples of data type

--- python_data_model.py
+++ python_data_model.py
@@ -107,7 +107,6 @@ points = 19
 total = 22
 'Correct answers: {:.2%}'.format(points / total)
 
-
 for align, text in zip('<^>', ['left', 'center', 'right']):
     print('{0:{fill}{align}16}'.format(text, fill=align, align=align))
 
@@ -115,7 +114,7 @@ octets = [192, 168, 0, 1]
 '{:02X}{:02X}{:02X}{:02X}'.format(*octets)
 
 width = 5
-for num in range(5,12):
+for num in range(5, 12):
     for base in 'dXob':
         print('{0:{width}{base}}'.format(num, base=base, width=width), end=' ')
     print()
