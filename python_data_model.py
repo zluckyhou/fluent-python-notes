57a9729c5b2c3741ca4c6d98e61e67b72ac076dc
zlucky <zluckyhou@163.com>
2017-07-01 23:39:27 +0800

    update class FrenchDeck

--- python_data_model.py
+++ python_data_model.py
@@ -1,5 +1,7 @@
 # -*- coding:utf-8 -*-
 
+import os
+from math import hypot
 import collections
 from random import choice
 
@@ -41,6 +43,79 @@ def spades_high(card):
     rank_value = FrenchDeck.ranks.index(card.rank)
     return rank_value * len(suits_values) + suits_values[card.suit]
 
-for card in sorted(deck,key=spades_high):
+
+for card in sorted(deck, key=spades_high):
     print(card)
 
+repr('2')
+str('2')
+
+
+class Vector:
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return 'Vector(%r, %r)' % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __bool__(self):
+        return bool(abs(self))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, scalar):
+        return Vector(self.x * scalar, self.y * scalar)
+
+
+divmod(20, 8)
+t = (20, 8)
+divmod(*t)
+
+_, filename = os.path.split('/home/luciano/.ssh/idrsa.pub')
+filename
+
+# str.format
+print('{:15} | {:^9} | {:^9}'.format('', 'lat.', 'long.'))
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x, self.y = x, y
+
+    def __str__(self):
+        return 'Point({self.x}, {self.y})'.format(self=self)
+
+    __repr__ = __str__
+
+
+Point(4, 2)
+# format also supports binary numbers
+"int: {0:d};  hex: {0:x};  oct: {0:o};  bin: {0:b}".format(42)
+
+# with 0x, 0o, or 0b as prefix:
+"int: {0:d};  hex: {0:#x};  oct: {0:#o};  bin: {0:#b}".format(42)
+
+# expressing a percentage
+points = 19
+total = 22
+'Correct answers: {:.2%}'.format(points / total)
+
+
+for align, text in zip('<^>', ['left', 'center', 'right']):
+    print('{0:{fill}{align}16}'.format(text, fill=align, align=align))
+
+octets = [192, 168, 0, 1]
+'{:02X}{:02X}{:02X}{:02X}'.format(*octets)
+
+width = 5
+for num in range(5,12):
+    for base in 'dXob':
+        print('{0:{width}{base}}'.format(num, base=base, width=width), end=' ')
+    print()
